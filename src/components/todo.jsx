9c535a7a46dd7476060d5e136b1f6a7884ba8c0b
huzaifa1012 <www.pakteach@gmail.com>
2022-11-30 00:40:54 +0500

    ui of app

---
+++ src/components/todo.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "../components/todo.css"
+export default function Todo() {
+ 
+ return (<>
+  
+  <div className="wrap"> 
+  <div className="main">
+    <div className="head">
+  <h1 className="todos">Todos</h1>
+  <input type="text" className="todo-inp" />
+  
+  <button className="add-but">Save</button>
+    </div>
+    <div className="todo-list">
+
+  <h3>hello</h3>
+  <h3>hello</h3>
+  <h3>hello</h3>
+  <h3>hello</h3>
+  <h3>hello</h3>
+  <h3>hello</h3>
+  <h3>hello</h3>
+  <h3>hello</h3>
+  <h3>hello</h3>
+  <h3>hello</h3>
+  <h3>hello</h3>
+  <h3>hello</h3>
+  <h3>hello</h3>
+  <h3>hello</h3>
+  <h3>hello</h3>
+    </div>
+  </div>
+  </div>  
+  
+  
+  </>);
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import Todo from './components/todo'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
+
   <React.StrictMode>
-    <App />
+
+  <Todo/>
+  
   </React.StrictMode>
 );
 
